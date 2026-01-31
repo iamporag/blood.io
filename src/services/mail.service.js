48a7b3a6cd098956bb2760a80ceaad16030d0911
iamporag <sakibporag24@gmail.com>
2026-01-31 15:43:53 +0600

    fix otp verification

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -1,12 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "smtp.gmail.com",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
 });
+console.log("MAIL_USER:", process.env.MAIL_USER);
+console.log("MAIL_PASS exists:", !!process.env.MAIL_PASS);
 
 async function sendVerificationEmail(email, code) {
   await transporter.sendMail({
